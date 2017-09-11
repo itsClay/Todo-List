0ca51bb7bb9c973f5034bf8352e6f1ba37d50219
Clay Shaw <Claydshaw@gmail.com>
2017-09-11 13:32:36 -0700

    Initial commit

---
+++ frontend/reducers/todosReducer.js
@@ -0,0 +1,15 @@
+const initialState = [];
+
+const todosReducer = (state = initialState, action) => {
+  switch (action.type) {
+    // case RECEIVE_TODOS:
+    //  return [...state, action.]
+    // case RECEIVE_TODO:
+    //
+    default:
+      return state;
+  }
+};
+
+
+export default todosReducer;
